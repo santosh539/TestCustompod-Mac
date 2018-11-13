a4879f39a3427cadce7f3ea6e3e06e81a3375c88
Santhosh Gandekattera <sgandekattera@innominds.com>
2018-11-13 13:09:32 +0530

    initial commit

---
+++ source/podTest.cpp
@@ -0,0 +1,17 @@
+//
+//  podTest.cpp
+//  Custompod-Mac
+//
+//  Created by Santhosh Gandekattera on 13/11/18.
+//  Copyright © 2018 Santhosh Gandekattera. All rights reserved.
+//
+
+#include "podTest.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
---
+++ source/podTest.hpp
@@ -0,0 +1,14 @@
+//
+//  podTest.hpp
+//  Custompod-Mac
+//
+//  Created by Santhosh Gandekattera on 13/11/18.
+//  Copyright © 2018 Santhosh Gandekattera. All rights reserved.
+//
+
+#ifndef podTest_hpp
+#define podTest_hpp
+
+#include <stdio.h>
+
+#endif /* podTest_hpp */
